f3327d1126ac58330a9c9b170895c4c2a4277198
semibroiled <amitavcmostafa@gmx.de>
2021-08-11 13:01:14 +0200

    first commit

---
+++ ttd2vr1.py
@@ -0,0 +1,15 @@
+'''
+This should engable you to solve the
+first Vorrechenübung and understand 
+the involved dependencies 
+
+ 
+'''
+
+
+y_i = [N_i/N, V_i/V, p_i/p] #Molanteil 
+
+x_i = M_i / M #Massenanteil 
+
+x_i = y_i * ()
+
